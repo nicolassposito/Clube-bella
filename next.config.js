a090f9e839f4b4f58923c10c3380ce7e25ca7411
Nicolas Sposito <noni.sposito@gmail.com>
2023-12-28 02:09:04 -0300

    hero

--- next.config.js
+++ next.config.js
@@ -2,3 +2,9 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+module.exports = {
+    images: {
+        domains: ['d2r9epyceweg5n.cloudfront.net'],
+    },
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Header } from "@/components/header/header";
+import Image from "next/image";
 
 interface HomePageProps {};
 
@@ -7,7 +8,20 @@ export default function HomePage({}: HomePageProps) {
     <>
     <Header></Header>
     <main className="flex flex-1">
-        <h1>Lorem</h1>
+    <Image
+          src='https://d2r9epyceweg5n.cloudfront.net/stores/001/608/438/rte/banner.a63d3389e2f7c9952cd6.png'
+          className="w-full md:block hidden"
+          alt="Home"
+          height={600}
+          width={1920}
+        />
+        <Image
+          src='https://d2r9epyceweg5n.cloudfront.net/stores/001/608/438/rte/banner%20mob.ba4336896001ad502bbb.png'
+          className="w-full md:hidden"
+          alt="Home"
+          height={760}
+          width={1000}
+        />
     </main>
     </>
   );
