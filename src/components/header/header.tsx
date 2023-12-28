859979eb443fe70f57dcd6d16bc43f227c17de9b
Nicolas Sposito <noni.sposito@gmail.com>
2023-12-27 23:47:48 -0300

    hover card

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -9,8 +9,14 @@ import {
   NavigationMenuTrigger,
   NavigationMenuViewport,
 } from "@/components/ui/navigation-menu";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
 import Image from "next/image";
-import logo from "..//..//..//public/images/logo-300.png";
+import logo from "..//..//..//public/images/logo-600.png";
+import logoB from "..//..//..//public/images/logo-b.png";
 import { Button } from "@/components/ui/button";
 import { UserRound, Bell } from "lucide-react";
 
@@ -19,15 +25,24 @@ interface HeaderProps {}
 export function Header({}: HeaderProps) {
   return (
     <header
-      className="flex items-center justify-between px-3 py-2"
+      className="flex items-center justify-between px-2 pr-3 py-3"
       style={{ boxShadow: "rgba(230, 109, 153, 0.4) 0px -3px 15px 1px;" }}
     >
       <div className="flex">
-        <Image src={logo} width={260} alt="Picture of the author" />
-        <NavigationMenu>
+        <Image
+          src={logo}
+          className="md:w-56 md:block hidden"
+          alt="Picture of the author"
+        />
+        <Image
+          src={logoB}
+          className="w-16 md:hidden"
+          alt="Picture of the author"
+        />
+        <NavigationMenu className="ml-0.5 md:ml-3">
           <NavigationMenuList>
             <NavigationMenuItem>
-              <NavigationMenuTrigger className="text-neutral-700">
+              <NavigationMenuTrigger className="text-neutral-700 bg-transparent md:px-3 px-2">
                 Menu
               </NavigationMenuTrigger>
               <NavigationMenuContent className="p-3 flex flex-col">
@@ -42,13 +57,29 @@ export function Header({}: HeaderProps) {
         </NavigationMenu>
       </div>
       <div className="flex items-center">
-        <Button className="bg-pink-500 hover:bg-rose-400">
+        <Button className="bg-pink-500 hover:bg-rose-400 md:px-4 px-3">
           <UserRound width={16} className="mr-1" />
-          Login
+          <span className="text-sm">Login</span>
         </Button>
         <div className="ml-3 relative">
-          <Bell className="bg-neutral-200 p-2.5 rounded-full text-neutral-600 cursor-pointer" height={40} width={40}/>
-          <div className="absolute bg-pink-400 text-white flex items-center justify-center w-5 h-5 text-xs rounded-full font-medium" style={{right: '-5px', bottom: '-6px'}}>1</div>
+          <HoverCard>
+            <HoverCardTrigger>
+              <Bell
+                className="bg-neutral-200 p-2.5 rounded-full text-neutral-600 cursor-pointer"
+                height={40}
+                width={40}
+              />
+              <div
+                className="absolute bg-pink-400 text-white flex items-center justify-center w-5 h-5 text-xs rounded-full font-medium"
+                style={{ right: "-5px", bottom: "-6px" }}
+              >
+                1
+              </div>
+            </HoverCardTrigger>
+            <HoverCardContent>
+              Lorem ipsum dolor sit amet consectetur adipisicing 
+            </HoverCardContent>
+          </HoverCard>
         </div>
       </div>
     </header>
