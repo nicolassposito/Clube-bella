df219fa86c2dbc9bcfdba1ab3a3bc286231452ad
Nicolas Sposito <noni.sposito@gmail.com>
2023-12-30 20:19:09 -0300

    sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -10,6 +10,7 @@ import logo from "..//..//public/images/logo-b.png";
 import clube from "..//..//public/images/clube.png";
 import Image from "next/image";
 import './sidebar.css'
+import Link from "next/link";
 
 interface SidebarProps {}
 
@@ -19,12 +20,12 @@ export function Sidebar({}: SidebarProps) {
     <aside
       className={cn(
         "hidden md:flex flex-col gap-5 h-screen top-0 py-4 px-2 sticky border-r",
-        isSidebarExpanded ? "w-60" : "w-20"
+        isSidebarExpanded ? "w-64" : "w-16"
       )}
     >
       <div className="flex items-center overflow-clip">
-        <Image src={logo} alt="Home" width={60} />
-        <Image src={clube} className="ml-1" alt="Home" width={90} />
+        <Image src={logo} alt="Home" width={47} />
+        <Image src={clube} className="ml-1" alt="Home" width={80} />
       </div>
 
       <div className="flex flex-col gap-6">
@@ -39,7 +40,7 @@ export function Sidebar({}: SidebarProps) {
       </nav> */}
 
       <div className="flex flex-col gap-3">
-      <div className="flex items-center gap-3 px-3 overflow-clip py-2 rounded sidebar-item active">
+      <Link href='/dashboard' className="flex items-center gap-4 px-3 overflow-clip py-2 rounded sidebar-item active">
           <UserCheck className="shrink-0 w-5 aspect-square text-primary" />
           <div className="flex flex-col whitespace-nowrap">
             <span className="text-zinc-800 font-semibold text-sm">
@@ -49,8 +50,8 @@ export function Sidebar({}: SidebarProps) {
               Gerencie sua assinatura
             </span>
           </div>
-        </div>
-        <div className="flex items-center gap-3 px-3 overflow-clip py-2 rounded sidebar-item">
+        </Link>
+        <Link href='/dashboard/preferencias' className="flex items-center gap-4 px-3 overflow-clip py-2 rounded sidebar-item">
           <Settings className="shrink-0 w-5 aspect-square text-primary" />
           <div className="flex flex-col whitespace-nowrap">
             <span className="text-zinc-800 font-semibold text-sm">
@@ -60,8 +61,8 @@ export function Sidebar({}: SidebarProps) {
               Preferências de recebimento
             </span>
           </div>
-        </div>
-        <div className="flex items-center gap-3 px-3 overflow-clip py-2 rounded sidebar-item">
+        </Link>
+        <Link href='/dashboard/troca' className="flex items-center gap-4 px-3 overflow-clip py-2 rounded sidebar-item">
           <RefreshCcw className="shrink-0 w-5 aspect-square text-primary" />
           <div className="flex flex-col whitespace-nowrap">
             <span className="text-zinc-800 font-semibold text-sm">
@@ -71,7 +72,7 @@ export function Sidebar({}: SidebarProps) {
               Solicitação de troca
             </span>
           </div>
-        </div>
+        </Link>
 
         <ThemeSwitcher />
         <SidebarExpandToggle />
