efbac1bb27af3b44267a237bbccaedef0bc93430
Nicolas Sposito <noni.sposito@gmail.com>
2023-12-30 16:02:14 -0300

    redirect

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,17 @@
 import { cookies } from "next/headers";
+import { permanentRedirect } from 'next/navigation';
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 
-export default async function WatchList(){
+export default async function Dashboard(){
     const cookieStore = cookies();
     const supabase = createServerComponentClient({cookies: () => cookieStore});
     const {data: {session}} = await supabase.auth.getSession();
     const user = session?.user;
-
     if(user){
         console.log("logado");
     } else{
         console.log("nao logado");
+        permanentRedirect('/auth/login')
     }
 
     return (
