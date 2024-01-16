88b68464b14e2db44bfd4f2363babd1d9970e410
Nicolas Sposito <noni.sposito@gmail.com>
2024-01-16 19:14:14 -0300

    useRouter

--- src/app/dashboard/useIsLogged.ts
+++ src/app/dashboard/useIsLogged.ts
@@ -1,9 +1,14 @@
 "use client"
 
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from 'next/navigation'
 
 export async function useIsLogged() {
+  const router = useRouter()
   const supabase = createClientComponentClient()
             const { data: { user } } = await supabase.auth.getUser()
+            if(!user){
+              router.push('/auth/login');
+            }
   return !!user;
 }
