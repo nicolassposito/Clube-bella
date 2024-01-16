beafb47103743f5e946fc72eff3efb52edb328c3
Nicolas Sposito <noni.sposito@gmail.com>
2024-01-16 20:27:23 -0300

    preferencias

--- src/app/dashboard/preferencias/page.tsx
+++ src/app/dashboard/preferencias/page.tsx
@@ -1,26 +1,25 @@
-import { cookies } from "next/headers";
-import { permanentRedirect } from "next/navigation";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+"use client";
+import React from "react";
 import { Sidebar } from "@/components/sidebar";
+import { useIsLogged } from "../useIsLogged";
+import { Toggle } from "@/components/ui/toggle";
 
-export default async function Dashboard() {
-  const cookieStore = cookies();
-  const supabase = createServerComponentClient({ cookies: () => cookieStore });
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
-  const user = session?.user;
-  if (user) {
-    console.log("logado");
-  } else {
-    console.log("nao logado");
-    permanentRedirect("/auth/login");
-  }
-
+export default function Preferences() {
+  useIsLogged();
   return (
     <>
       <div className="flex">
         <Sidebar />
+        <div className="w-full h-screen overflow-scroll relative">
+          <div>
+            <div className="text-center">
+              <h1 className="text-2xl">Preferências de Recebimento</h1>
+              <p>Aqui você pode controlar as preferências do seu perfil.</p>
+              <p>Certifique-se de escolher as opções que mais te agradam 😁.</p>
+            </div>
+            <Toggle>A</Toggle>
+          </div>
+        </div>
       </div>
     </>
   );
