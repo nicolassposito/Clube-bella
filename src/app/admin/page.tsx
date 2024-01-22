8d1c93dff2f3f9f27cbd829813445c2d1a9f85ec
Nicolas Sposito <noni.sposito@gmail.com>
2024-01-22 20:51:23 -0300

    fix

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,50 +1,17 @@
 "use client"
 
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import Image from "next/image";
-import logo from "..//..//..//public/images/logo-b.png";
-import {
-  CaretSortIcon,
-  ChevronDownIcon,
-  DotsHorizontalIcon,
-} from "@radix-ui/react-icons";
-import {
-  ColumnDef,
-  SortingState,
-  VisibilityState,
-  flexRender,
-  useReactTable,
-  getCoreRowModel,
-} from "@tanstack/react-table";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
+import logo from "../../../public/images/logo-b.png";
+import { CaretSortIcon, ChevronDownIcon, DotsHorizontalIcon } from "@radix-ui/react-icons";
+import { ColumnDef, SortingState, VisibilityState, flexRender, useReactTable, getCoreRowModel } from "@tanstack/react-table";
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
-import {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 
 const supabase = createClientComponentClient();
 
@@ -111,135 +78,135 @@ async function fetchData(filterEmail: string): Promise<Profile[]> {
 
   return validProfilesResults.filter((profile): profile is Profile => profile !== null);
 }
-  
-  export const columns: ColumnDef<Profile>[] = [
-    {
-      accessorKey: "fullname",
-      header: "Nome",
-      cell: ({ row }) => <div>{row.getValue("fullname")}</div>,
-    },
-    {
-      accessorKey: "email",
-      header: "Email",
-      cell: ({ row }) => <div>{row.getValue("email")}</div>,
-    },
-    {
-      accessorKey: "ultimoEnvio",
-      header: "Último Envio",
-      cell: ({ row }) => <div>{row.getValue("ultimoEnvio")}</div>,
-    },
-    {
-        id: 'actions',
-        cell: ({ row }) => {
-            const [codigoRastreio, setCodigoRastreio] = React.useState('');
-            const [preferences, setPreferences] = React.useState({ endereco: '', complemento: '', cor: '', tamanho: '' });
-            const [isLoading, setIsLoading] = React.useState(false);
-      
-          const adicionarEnvio = async () => {
-            const { data, error } = await supabase
-              .from('envios')
-              .insert([
-                { id: row.original.id, codigo_rastreio: codigoRastreio, data_postagem: new Date() }
-              ]);
-      
-            if (error) {
-              console.error('Erro ao adicionar envio:', error);
-            } else {
-              console.log('Envio adicionado com sucesso:', data);
-              setCodigoRastreio('');
-            }
-          };
-        
-            const fetchPreferences = async () => {
-              setIsLoading(true);
-              const { data, error } = await supabase
-                .from('preferences')
-                .select('cor, tamanho , endereco, complemento')
-                .eq('id', row.original.id)
-                .single();
-        
-              if (error) {
-                setPreferences({ endereco: 'N/A', complemento: 'N/A', tamanho: 'N/A', cor: 'N/A' });
-              } else {
-                setPreferences(data || { endereco: 'N/A', complemento: 'N/A', tamanho: 'N/A', cor: 'N/A' });
-              }
-              setIsLoading(false);
-            };
-        
-            return (
-              <div className="flex items-center justify-end">
-                <AlertDialog>
-                  <AlertDialogTrigger asChild onClick={fetchPreferences}>
-                    <Button variant="ghost" className="h-8 w-8 p-0">
-                      <DotsHorizontalIcon className="h-5 w-5" />
-                    </Button>
-                  </AlertDialogTrigger>
-                  <AlertDialogContent>
-                    <AlertDialogHeader>
-                      <AlertDialogTitle>Adicionar Código de Envio</AlertDialogTitle>
-                    </AlertDialogHeader>
-                    <div className="flex flex-col p-4">
-                      {isLoading ? (
-                        <p>Carregando...</p>
-                      ) : (
-                        <>
-                          <div className="py-2">
-                            <p className="text-base">Endereço: {preferences.endereco}</p>
-                            <p>Complemento: {preferences.complemento}</p>
-                          </div>
-                          <div className="py-2">
-                            <h1 className="text-base">Preferências de Recebimento:</h1>
-                            <p>Cor: {preferences.cor}</p>
-                            <p>Tamanho: {preferences.tamanho}</p>
-                          </div>
-                          <Input
-                            placeholder="Código de envio"
-                            value={codigoRastreio}
-                            onChange={(e) => setCodigoRastreio(e.target.value)}
-                          />
-                          <Button onClick={adicionarEnvio} className="mt-4 bg-rose-400 text-white hover:bg-rose-500">
-                            Adicionar Envio
-                          </Button>
-                        </>
-                      )}
-                    </div>
-                    <AlertDialogFooter>
-                      <AlertDialogCancel asChild>
-                        <Button variant="ghost">Cancelar</Button>
-                      </AlertDialogCancel>
-                    </AlertDialogFooter>
-                  </AlertDialogContent>
-                </AlertDialog>
-              </div>
-            );
-          },
-      },
-  ];
-  
-  export default function DataTableDemo() {
-    const [data, setData] = React.useState<Profile[]>([]);
-    const [filterEmail, setFilterEmail] = React.useState("");
-    const [sorting, setSorting] = React.useState<SortingState>([]);
-    const [columnVisibility, setColumnVisibility] = React.useState<VisibilityState>({});
-  
-    React.useEffect(() => {
-      fetchData(filterEmail).then(fetchedData => {
-        setData(fetchedData);
-      });
-    }, [filterEmail]);
-  
-    const table = useReactTable({
-      data,
-      columns,
-      onSortingChange: setSorting,
-      getCoreRowModel: getCoreRowModel(),
-      state: {
-        sorting,
-        columnVisibility,
-      },
-    });
-  
 
+function ActionsCell({ profile }: { profile: Profile }) {
+  const [codigoRastreio, setCodigoRastreio] = useState('');
+  const [preferences, setPreferences] = useState({ endereco: '', complemento: '', cor: '', tamanho: '' });
+  const [isLoading, setIsLoading] = useState(false);
+
+  const adicionarEnvio = async () => {
+    const { data, error } = await supabase
+      .from('envios')
+      .insert([
+        { id: profile.id, codigo_rastreio: codigoRastreio, data_postagem: new Date() }
+      ]);
+
+    if (error) {
+      console.error('Erro ao adicionar envio:', error);
+    } else {
+      console.log('Envio adicionado com sucesso:', data);
+      setCodigoRastreio('');
+    }
+  };
+
+  const fetchPreferences = async () => {
+    setIsLoading(true);
+    const { data, error } = await supabase
+      .from('preferences')
+      .select('cor, tamanho , endereco, complemento')
+      .eq('id', profile.id)
+      .single();
+
+    if (error) {
+      setPreferences({ endereco: 'N/A', complemento: 'N/A', tamanho: 'N/A', cor: 'N/A' });
+    } else {
+      setPreferences(data || { endereco: 'N/A', complemento: 'N/A', tamanho: 'N/A', cor: 'N/A' });
+    }
+    setIsLoading(false);
+  };
+
+  return (
+    <div className="flex items-center justify-end">
+      <AlertDialog>
+        <AlertDialogTrigger asChild onClick={fetchPreferences}>
+          <Button variant="ghost" className="h-8 w-8 p-0">
+            <DotsHorizontalIcon className="h-5 w-5" />
+          </Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Adicionar Código de Envio</AlertDialogTitle>
+          </AlertDialogHeader>
+          <div className="flex flex-col p-4">
+            {isLoading ? (
+              <p>Carregando...</p>
+            ) : (
+              <>
+                <div className="py-2">
+                  <p className="text-base">Endereço: {preferences.endereco}</p>
+                  <p>Complemento: {preferences.complemento}</p>
+                </div>
+                <div className="py-2">
+                  <h1 className="text-base">Preferências de Recebimento:</h1>
+                  <p>Cor: {preferences.cor}</p>
+                  <p>Tamanho: {preferences.tamanho}</p>
+                </div>
+                <Input
+                  placeholder="Código de envio"
+                  value={codigoRastreio}
+                  onChange={(e) => setCodigoRastreio(e.target.value)}
+                />
+                <Button onClick={adicionarEnvio} className="mt-4 bg-rose-400 text-white hover:bg-rose-500">
+                  Adicionar Envio
+                </Button>
+              </>
+            )}
+          </div>
+          <AlertDialogFooter>
+            <AlertDialogCancel asChild>
+              <Button variant="ghost">Cancelar</Button>
+            </AlertDialogCancel>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </div>
+  );
+}
+
+export const columns: ColumnDef<Profile>[] = [
+  {
+    accessorKey: "fullname",
+    header: "Nome",
+    cell: ({ row }) => <div>{row.getValue("fullname")}</div>,
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+    cell: ({ row }) => <div>{row.getValue("email")}</div>,
+  },
+  {
+    accessorKey: "ultimoEnvio",
+    header: "Último Envio",
+    cell: ({ row }) => <div>{row.getValue("ultimoEnvio")}</div>,
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => <ActionsCell profile={row.original} />,
+  },
+];
+
+export default function DataTableDemo() {
+  const [data, setData] = useState<Profile[]>([]);
+  const [filterEmail, setFilterEmail] = useState("");
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+
+  useEffect(() => {
+    fetchData(filterEmail).then(fetchedData => {
+      setData(fetchedData);
+    });
+  }, [filterEmail]);
+
+  const table = useReactTable({
+    data,
+    columns,
+    onSortingChange: setSorting,
+    getCoreRowModel: getCoreRowModel(),
+    state: {
+      sorting,
+      columnVisibility,
+    },
+  });
   return (
     <div className="w-full container mx-auto">
       <div className="flex items-center justify-center">
