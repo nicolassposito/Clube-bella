cbaee47ea94321bc37666bb28fcc2388c99026f1
Nicolas Sposito <noni.sposito@gmail.com>
2024-01-23 13:17:59 -0300

    track ui

--- src/app/dashboard/rastreio/page.tsx
+++ src/app/dashboard/rastreio/page.tsx
@@ -54,21 +54,24 @@ export default function Rastreio() {
         <div className="w-full h-screen overflow-scroll relative">
           <div className="text-center px-3 mt-10 md:mt-0 md:absolute md:top-1/2 bottom-0 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2 flex flex-col md:inline-flex gap-4">
           <h1 className="text-2xl">Rastreamento de produto</h1>
-            
-            {pedidos.map((pedido, index) => (
-              <div key={index} className="pedido p-3 border rounded-xl">
-                <div className="text-center">
-                  <p className="flex items-center gap-1 text-base">
-                    <TruckIcon size={30} className="text-white bg-rose-400 rounded-full p-1"/>Seu pedido foi postado!
-                  </p>
-                  <p className="py-2 text-sm text-xinc-600">Código de rastreio:</p>
-                  <button className="bg-zinc-100 py-1.5 px-3 rounded">{pedido.codigo_rastreio}</button>
+          {pedidos.length > 0 ? (
+              pedidos.map((pedido, index) => (
+                <div key={index} className="pedido p-3 border rounded-xl">
+                  <div className="text-center">
+                    <p className="flex items-center gap-1 text-base">
+                      <TruckIcon size={30} className="text-rose-400 p-1"/>Seu pedido foi postado!
+                    </p>
+                    <p className="py-2 text-sm text-xinc-600">Código de rastreio:</p>
+                    <button className="bg-zinc-100 py-1.5 px-3 rounded">{pedido.codigo_rastreio}</button>
+                  </div>
+                  <div className="mt-5 text-sm text-zinc-500">
+                    Pedido postado dia <span className="text-rose-500">{pedido.data_postagem}</span>
+                  </div>
                 </div>
-                <div className="mt-5 text-sm text-zinc-500">
-                  Pedido postado dia <span className="text-rose-500">{pedido.data_postagem}</span>
-                </div>
-              </div>
-            ))}
+              ))
+            ) : (
+              <h1 className="border p-3 rounded flex items-center gap-2 justify-center"><Truck size={30} className="text-rose-400"/>Nenhum pedido foi enviado.</h1>
+            )}
           </div>
         </div>
       </div>
