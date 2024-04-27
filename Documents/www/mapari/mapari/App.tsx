52d2c74b640c09f4cc73f80594790d4ed36caad1
Nicolas Sposito <noni.sposito@gmail.com>
2024-04-27 02:38:00 -0300

    first commit

---
+++ Documents/www/mapari/mapari/App.tsx
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>orselli!</Text>
+      <MapView />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
